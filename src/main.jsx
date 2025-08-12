6070c35033ebaf91e6f5618ff5fa0f9cd9a526a9
JohnAturN99 <jonathanteojunyan@gmail.com>
2025-08-12 16:45:26 +0800

    Deploy latest changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { registerSW } from 'virtual:pwa-register';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+
+const updateSW = registerSW({
+  immediate: true,
+  onNeedRefresh() {
+    if (confirm('A new version is available. Refresh now?')) {
+      updateSW(true); // apply update and reload
+    }
+  },
+  onOfflineReady() {
+    console.log('App is ready to work offline');
+  },
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
